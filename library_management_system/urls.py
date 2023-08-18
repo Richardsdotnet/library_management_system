8ce0fd64c0d04bc3bf44ad22323004c76a973eca
Akaabiam Richards <richardsakaabiam@gmail.com>
2023-08-18 10:51:48 +0100

    updated

--- library_management_system/urls.py
+++ library_management_system/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
     path('books/', include("books.urls")),
-    path('users/', include("users.urls"))
+    path('users/', include("users.urls")),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 
 ]
