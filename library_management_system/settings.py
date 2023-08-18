e9ed91448d774dab4c2ec876ad134dd4f0e8a1d9
Akaabiam Richards <richardsakaabiam@gmail.com>
2023-08-18 10:44:26 +0100

    updated

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djoser',
     'rest_framework',
     "debug_toolbar",
     'django_filters',
